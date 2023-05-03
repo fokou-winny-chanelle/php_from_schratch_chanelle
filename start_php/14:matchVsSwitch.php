b40feec2f0d6c6e2a61605be9d0189d7a462895e
fokou <fokouchanelle@gmail.com>
2023-05-03 14:08:53 +0100

    difference btw match and switchcase

---
+++ start_php/14:matchVsSwitch.php
@@ -0,0 +1,23 @@
+<?php 
+    // switch case
+    $weekday = "Fri";
+    switch($weekday) 
+    {
+        case "Mon":
+            echo 'Monday';
+            break;
+        case "Fri":
+            echo 'Friday' ;
+            break;
+        default:
+            echo 'Others' ;
+    }
+    echo "<br><br>";
+    // match
+    match($weekday)
+    {
+        "Mon" => print 'Monday',
+        "Fri" => print 'Friday' ,
+        default => 'others',
+    }
+?>
